b6073d2969d3e41a2a44e784f277f53d99b9227d
Mike <big.ben@gmail.com>
2023-02-13 00:24:41 +0300

    save

---
+++ Task66/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N
+
+Console.Clear();
+Console.Write("Введите число m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write($"Сумма натуральных элементов равна: {SumElements(m, n)}");
+
+string f(int m, int n)
+{
+    if (m == n)
+        return $"{m}";
+    return f(m, n - 1) + $", {n}";
+}
+
+int SumElements(int m, int n)
+{
+    if (m == n)
+        return n;
+    return n + SumElements(m, n - 1);
+}
---
+++ Task68/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n
+
+Console.Clear();
+Console.Write("Введите число m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write($"A(m,n) равна: {FuncAkkerman(m, n)}");
+
+string f(int m, int n)
+{
+    if (m == n)
+        return $"{m}";
+    return f(m, n - 1) + $", {n}";
+}
+
+int FuncAkkerman(int m, int n)
+{
+    if (m == 0)
+        return n + 1;
+    if (m > 0 && n == 0)
+        return FuncAkkerman(m - 1, 1);
+    else
+        return FuncAkkerman(m - 1, FuncAkkerman(m, n - 1));
+}
